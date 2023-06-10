fb5893ca3c3df0b2b8dc67621050ca401fedf2dd
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-06-10 13:41:16 -0300

    Salvando o user criado

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -14,5 +14,25 @@ export class UserController {
     if(userExist) {
       return res.status(400).json({ message: "User already exists" })
     }
+
+    const newUser = new User();
+
+    newUser.name = req.body.name;
+    newUser.email = req.body.email;
+    newUser.password_hash = req.body.password_hash;
+
+    try {
+      await userRepository.save(newUser);
+    } catch(err) {
+      return res.status(400).json({ message: "Something went wrong with the upload" })
+    }
+
+    const {id, name, email} = newUser;
+
+    return res.json({
+      id,
+      name, 
+      email
+    })
   }
 }
