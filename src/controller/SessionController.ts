74facf7366ff47633118ecf83e5e80cfdb9e7223
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-06-12 10:06:47 -0300

    Fazendo post de Sessions e exportando para routes

--- src/controller/SessionController.ts
+++ src/controller/SessionController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
+import * as bcrypt from "bcrypt";
 
 import { userRepository } from "./UserController";
 
 class SessionController {
   async store(req: Request, res: Response) {
-    const { email, password_hash } = req.body;
+    const { email, password } = req.body;
 
     const user = await userRepository.findOne({ where: { email } });
 
@@ -12,6 +13,18 @@ class SessionController {
       return res.status(400).json({ message: "User does not exist" });
     }
 
-    
+    if(!(await bcrypt.compare(password, user.password_hash))) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      id,
+      name,
+      email
+    });
   }
 }
+
+export default new SessionController();
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 
 import UserController from "../controller/UserController";
+import SessionController from "../controller/SessionController";
 
 export const routes = Router();
 
-routes.post("/users", UserController.store)
+// Users
+routes.post("/users", UserController.store);
+
+// Sessions
+routes.post("/sessions", SessionController.store);
